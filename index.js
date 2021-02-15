04d15d53fe438968e6d1a1f0f9e33a714ab6917c
Koko est <61022938+MatheusAznavour@users.noreply.github.com>
2021-02-15 20:27:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var rating = document.getElementsByClassName('ratingPont')[0]
+
+let func1 = Star01 = () =>{
+    const star1 = document.getElementsByClassName('star1')[0]
+    star1.style.background = '#E76363';
+    rating.innerText = ' 1'
+    
+}
+
+Star02 = () =>{
+    var star2 = document.getElementsByClassName('star2')[0]
+    star2.style.background = '#E76363';
+    rating.innerText = ' 2'
+}
+
+Star03 = () =>{
+    const star3 = document.getElementsByClassName('star3')[0]
+    star3.style.background = '#E76363';
+    rating.innerText = ' 3'
+}
+
+Star04 = () =>{
+    const star4 = document.getElementsByClassName('star4')[0]
+    star4.style.background = '#E76363';
+    rating.innerText = ' 4'
+}
+
+Star05 = () =>{
+    const star5 = document.getElementsByClassName('star5')[0]
+    star5.style.background += '#E76363';
+    rating.innerText = ' 5'
+}
+
